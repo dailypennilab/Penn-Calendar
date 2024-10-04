0b9563be8b176f3ef37670dd71a22ee0da82ee5e
Ranvith Adulla <ranvithadulla@gmail.com>
2024-10-03 21:55:25 -0400

    Update MongoDB connection credentials

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Student = require('../models/Student'); // Adjust paths as needed
 const Org = require('../models/Org');
 const router = express.Router();
--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON requests
 
 // MongoDB connection
-const CONNECTION_STRING = "mongodb+srv://ilabmanager:n7VkZ0SoHEjmQlBw@penncalendar.rjlgg.mongodb.net/?retryWrites=true&w=majority&appName=PennCalendar";
-mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+const CONNECTION_STRING = "mongodb+srv://ilabmanager:n7VkZ0SoHEjmQlBw@penncalendar.rjlgg.mongodb.net/calendardb?retryWrites=true&w=majority&appName=PennCalendar";
+mongoose.connect(CONNECTION_STRING)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
